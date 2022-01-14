362061f668fa3e4608c70b3b3eef1c26934ba007
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2022-01-14 16:20:40 +0100

    react starter code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import Pages from "./pages";
+
+//hooks
+import ScrollToTheTop from "./hooks/ScrollToTheTop";
+
+// Redux
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Alert } from "./components/Layout/Alert";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Alert />
+          <ScrollToTheTop />
+          <Pages />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
